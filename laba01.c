ab5e988d91d3336a139273e974507cad0f25f5c4
deathless0708 <138172790+deathless0708@users.noreply.github.com>
2025-10-04 12:45:20 +0000

    ghfgfh

--- laba01.c
+++ laba01.c
@@ -1,1 +1,18 @@
-fdgnfd,gnd,
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+
+int x = (int)'C';
+int main(void) {
+
+    printf("%d\n", x);
+    return 0;
+};
+
+int randNum = std.rand() % (126 - 32 + 1) + 32;
+
+int main(void) {
+
+    printf("%d\n", randNum);
+    return 0;
+};
